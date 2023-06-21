004ba1ed65645000a0cfb4ef3be1aad52c1e6f0a
Leonardo Ballestero <ballesteroleonardojose@gmail.com>
2023-06-20 20:18:19 -0500

    chore: first commit with base project

---
+++ src/main/java/com/interview/service/msproductmanagement/MsProductManagementV1Application.java
@@ -0,0 +1,13 @@
+package com.interview.service.msproductmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductManagementV1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MsProductManagementV1Application.class, args);
+  }
+
+}
